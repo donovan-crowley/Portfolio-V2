80502596c69753bb6a02a9aa78a785c7fcb1d09e
Donovan Crowley <donocrowley16@gmail.com>
2026-07-01 22:13:36 -0400

    finished experience. TODO: setup nav for project pages and provide descriptions

---
+++ src/pages/webscraper.jsx
@@ -0,0 +1,19 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+export default function Webscraper() {
+
+    return(
+        <section className="py-5 bg-black text-light">
+            <Container className="px-4">
+                <Row className="align-items-center justify-content-center">
+                    <Col md={{ span: 10, offset: 1 }} className="ms-md-2">
+                        <h2 className="font-monospace text-accent">/ blog</h2>
+                        <p className="font-monospace opacity-75">
+                            More to come with this!
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+}
